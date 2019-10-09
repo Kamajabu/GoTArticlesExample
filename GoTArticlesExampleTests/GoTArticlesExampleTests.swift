c399ac1dcfc0460518f0ed1bfb8429a142c02d81
Kamil Buczel <kamil.buczel@kamajabu.pl>
2019-10-09 18:14:50 +0200

    Initial Commit

---
+++ GoTArticlesExampleTests/GoTArticlesExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  GoTArticlesExampleTests.swift
+//  GoTArticlesExampleTests
+//
+//  Created by Kamil Buczel on 09/10/2019.
+//  Copyright © 2019 Kamajabu. All rights reserved.
+//
+
+import XCTest
+@testable import GoTArticlesExample
+
+class GoTArticlesExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
